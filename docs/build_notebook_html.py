3ff8d630ce2eda5f355a250f4d7e84b4e8c72383
Dan Gunter <dkgunter@lbl.gov>
2019-11-05 07:00:54 -0800

    building notebooks better

--- docs/build_notebook_html.py
+++ docs/build_notebook_html.py
@@ -4,7 +4,7 @@ import logging
 import pathlib
 import sys
 #
-import nbformat, nbconvert
+import nbformat
 from nbconvert.exporters import HTMLExporter
 from nbconvert.writers import FilesWriter
 from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
@@ -19,13 +19,16 @@ class NotebookError(Exception):
     pass
 
 
-def notebooks_to_html(srcdir=None, outdir=None):
+def notebooks_to_html(srcdir=None, outdir=None, options=None):
     _log.debug(f"looking for notebooks in {srcdir}")
 
     hexp = HTMLExporter()
     wrt = FilesWriter()
     wrt.build_directory = str(outdir)
-    ep = ExecutePreprocessor(timeout=600)
+    ep_kw = {}
+    if options["kernel"]:
+        ep_kw["kernel_name"] = options["kernel"]
+    ep = ExecutePreprocessor(timeout=600, **ep_kw)
 
     for entry in srcdir.iterdir():
         if entry.parts[-1].startswith("."):
@@ -33,14 +36,20 @@ def notebooks_to_html(srcdir=None, outdir=None):
             continue  # e.g. .ipynb_checkpoints
         if entry.is_dir():
             new_outdir = outdir / entry.parts[-1]
-            notebooks_to_html(srcdir=entry, outdir=new_outdir)
+            notebooks_to_html(srcdir=entry, outdir=new_outdir, options=options)
         elif entry.suffix == ".ipynb":
-            _log.info(f"converting '{entry}' into dir '{outdir}'")
+            html_target = str(outdir / entry.stem) + ".html"
+            _log.info(f"converting '{entry}' to '{html_target}'")
             nb = nbformat.read(str(entry), as_version=4)
             try:
                 ep.preprocess(nb, {'metadata': {'path': str(entry.parent)}})
-            except CellExecutionError as err:
-                raise NotebookError(f"execution error for '{entry}': {err}")
+            except (CellExecutionError, NameError) as err:
+                if options["continue"]:
+                    _log.error(f"execution of '{entry}' failed: {err}")
+                    _log.warning(f"continuing to generate partial HTML for '{entry}' "
+                                 f"in '{html_target}'")
+                else:
+                    raise NotebookError(f"execution error for '{entry}': {err}")
             (body, resources) = hexp.from_notebook_node(nb)
             wrt.write(body, resources, notebook_name=entry.stem)
 
@@ -48,12 +57,14 @@ def notebooks_to_html(srcdir=None, outdir=None):
 
 
 def main():
-    status_code = 0
     ap = argparse.ArgumentParser()
     ap.add_argument("source_dir")
     ap.add_argument("output_dir")
     ap.add_argument("-v", "--verbose", action="count", dest="vb", default=0)
+    ap.add_argument("--continue", dest="cont", action="store_true", default=False)
+    ap.add_argument("--kernel", dest="kernel", default="")
     args = ap.parse_args()
+
     if args.vb > 1:
         _log.setLevel(logging.DEBUG)
     elif args.vb > 0:
@@ -64,8 +75,12 @@ def main():
     outdir = pathlib.Path(args.output_dir)
     if not outdir.exists():
         _log.fatal(f"output directory does not exist: {outdir}")
+
+    options = {"continue": args.cont, "kernel": args.kernel}
+
+    status_code = 0
     try:
-        notebooks_to_html(srcdir=srcdir, outdir=outdir)
+        notebooks_to_html(srcdir=srcdir, outdir=outdir, options=options)
     except ValueError as err:
         _log.fatal(f"error converting notebooks: {err}")
         status_code = 1
