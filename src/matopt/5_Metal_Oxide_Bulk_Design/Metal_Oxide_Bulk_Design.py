c771b362b11ddda77c6f0696fff701e7a84d0263
Xiangyu Yin <xiangyuy@andrew.cmu.edu>
2020-02-20 15:14:50 -0500

    Modifications to the examples

--- src/matopt/5_Metal_Oxide_Bulk_Design/Metal_Oxide_Bulk_Design.py
+++ src/matopt/5_Metal_Oxide_Bulk_Design/Metal_Oxide_Bulk_Design.py
@@ -1,59 +1,62 @@
-from matopt import *
+import numpy as np
+from apps.matopt import *
 
-A = 4.0
-B = 4.0
-C = 4.0
-Lat = PerovskiteLattice(A,B,C)
+if __name__ == '__main__':
 
-nUnitCellsOnEdge = 2
-S = RectPrism(nUnitCellsOnEdge*A,
-              nUnitCellsOnEdge*B,
-              nUnitCellsOnEdge*C)
-S.shift(np.array([-0.01,-0.01,-0.01]))
-T = CubicTiling(S)
+    A = 4.0
+    B = 4.0
+    C = 4.0
+    Lat = PerovskiteLattice(A,B,C)
 
-Canv = Canvas.fromLatticeAndTilingScan(Lat,T)
-Atoms = [Atom('Ba'),Atom('Fe'),Atom('In'),Atom('O')]
+    nUnitCellsOnEdge = 2
+    S = RectPrism(nUnitCellsOnEdge*A,
+                  nUnitCellsOnEdge*B,
+                  nUnitCellsOnEdge*C)
+    S.shift(np.array([-0.01,-0.01,-0.01]))
+    T = CubicTiling(S)
 
-iDesiredConfs = [394,395,396,397,398,399,400,401,68,69,
-                 70,71,162,163,164,165,166,167,168,169]
-ConfDesigns = loadFromPDBs([str(i)+'.pdb' for i in iDesiredConfs],folder='./Confs')
-Confs = [Conf.Contents for Conf in ConfDesigns]
+    Canv = Canvas.fromLatticeAndTilingScan(Lat,T)
+    Atoms = [Atom('Ba'),Atom('Fe'),Atom('In'),Atom('O')]
 
-Sites = [i for i in range(len(Canv))]
-ASites = [i for i in Sites if Lat.isASite(Canv.Points[i])]
-BSites = [i for i in Sites if Lat.isBSite(Canv.Points[i])]
-OSites = [i for i in Sites if Lat.isOSite(Canv.Points[i])]
-pctLocalLB,pctLocalUB = 0,1
-pctGlobalLB,pctGlobalUB = 0.0,0.3
-LocalBounds = {(i,Atom('In')):(round(pctLocalLB*len(Canv.NeighborhoodIndexes[i])),
-                               round(pctLocalUB*len(Canv.NeighborhoodIndexes[i]))) for i in OSites}
-GlobalLB = round(pctGlobalLB*len(BSites))
-GlobalUB = round(pctGlobalUB*len(BSites))
+    iDesiredConfs = [394,395,396,397,398,399,400,401,68,69,
+                     70,71,162,163,164,165,166,167,168,169]
+    ConfDesigns = loadFromPDBs([str(i)+'.pdb' for i in iDesiredConfs],folder='./Confs')
+    Confs = [Conf.Contents for Conf in ConfDesigns]
 
-m = MatOptModel(Canv,Atoms,Confs)
+    Sites = [i for i in range(len(Canv))]
+    ASites = [i for i in Sites if Lat.isASite(Canv.Points[i])]
+    BSites = [i for i in Sites if Lat.isBSite(Canv.Points[i])]
+    OSites = [i for i in Sites if Lat.isOSite(Canv.Points[i])]
+    pctLocalLB,pctLocalUB = 0,1
+    pctGlobalLB,pctGlobalUB = 0.0,0.3
+    LocalBounds = {(i,Atom('In')):(round(pctLocalLB*len(Canv.NeighborhoodIndexes[i])),
+                                   round(pctLocalUB*len(Canv.NeighborhoodIndexes[i]))) for i in OSites}
+    GlobalLB = round(pctGlobalLB*len(BSites))
+    GlobalUB = round(pctGlobalUB*len(BSites))
 
-m.Yik.rules.append(FixedTo(1,sites=ASites,site_types=[Atom('Ba')]))
-m.Yik.rules.append(FixedTo(1,sites=OSites,site_types=[Atom('O')]))
-m.Yik.rules.append(FixedTo(0,sites=BSites,site_types=[Atom('Ba'),Atom('O')]))
-m.Yi.rules.append(FixedTo(1,sites=BSites))
+    m = MatOptModel(Canv,Atoms,Confs)
 
-m.addGlobalDescriptor('Activity',
-                      rules=EqualTo(SumSitesAndConfs(m.Zic,coefs=1/len(OSites),sites_to_sum=OSites)))
-m.addGlobalTypesDescriptor('GlobalBudget',bounds=(GlobalLB,GlobalUB),
-                           rules=EqualTo(SumSites(m.Yik,
-                                                  site_types=[Atom('In')],
-                                                  sites_to_sum=BSites)))
-m.addSitesTypesDescriptor('LocalBudget',bounds=LocalBounds,
-                          rules=EqualTo(SumNeighborSites(m.Yik,
-                                                         sites=OSites,
-                                                         site_types=[Atom('In')])))
+    m.Yik.rules.append(FixedTo(1,sites=ASites,site_types=[Atom('Ba')]))
+    m.Yik.rules.append(FixedTo(1,sites=OSites,site_types=[Atom('O')]))
+    m.Yik.rules.append(FixedTo(0,sites=BSites,site_types=[Atom('Ba'),Atom('O')]))
+    m.Yi.rules.append(FixedTo(1,sites=BSites))
+
+    m.addGlobalDescriptor('Activity',
+                          rules=EqualTo(SumSitesAndConfs(m.Zic,coefs=1/len(OSites),sites_to_sum=OSites)))
+    m.addGlobalTypesDescriptor('GlobalBudget',bounds=(GlobalLB,GlobalUB),
+                               rules=EqualTo(SumSites(m.Yik,
+                                                      site_types=[Atom('In')],
+                                                      sites_to_sum=BSites)))
+    m.addSitesTypesDescriptor('LocalBudget',bounds=LocalBounds,
+                              rules=EqualTo(SumNeighborSites(m.Yik,
+                                                             sites=OSites,
+                                                             site_types=[Atom('In')])))
 
 
-D = m.maximize(m.Activity,tilim=360)
+    D = m.maximize(m.Activity,tilim=360)
 
-if(D is not None):
-    for i,c in m.Zic.keys():
-        if(m.Zic.values[i,c] > 0.5):
-            D.setContent(i,Atom('S'))
-    D.toCFG('result.cfg',BBox=S)
+    if(D is not None):
+        for i,c in m.Zic.keys():
+            if(m.Zic.values[i,c] > 0.5):
+                D.setContent(i,Atom('S'))
+        D.toCFG('result.cfg',BBox=S)
