74a72f787482a4b1c5078b8334a3b0ef33d8fa38
Andrew Lee <andrew.lee@netl.doe.gov>
2020-03-02 17:02:59 -0500

    Updating to generic_models

--- src/tutorials/deprecated/Tutorial_1_Basic_Flowsheets.py
+++ src/tutorials/deprecated/Tutorial_1_Basic_Flowsheets.py
@@ -24,11 +24,11 @@ from idaes.core import FlowsheetBlock
 from idaes.core.util.model_statistics import report_statistics
 
 # Import Unit Model Modules
-import idaes.core_lib.properties.examples.saponification_thermo as thermo_props
-import idaes.core_lib.properties.examples.saponification_reactions as reaction_props
+import idaes.generic_models.properties.examples.saponification_thermo as thermo_props
+import idaes.generic_models.properties.examples.saponification_reactions as reaction_props
 
 # Import Unit Model Modules
-from idaes.core_lib.unit_models import CSTR
+from idaes.generic_models.unit_models import CSTR
 
 
 def main():
