39100b9a8ba775108510ad6b8b987d2ebc9b57c3
Keith Beattie <KSBeattie@lbl.gov>
2020-04-13 22:36:43 +0000

    examples-dev 1.5_rel branch as of cd402c2 in prep for 1.5.0 release

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = 'DOE Institute for the Design of Advanced Energy Systems (IDAES), 20
 author = 'Dan Gunter'
 
 # The full version, including alpha/beta/rc tags
-release = '1.5.0rc2'
+release = '1.5.0'
 
 
 # For Read the Docs theme
