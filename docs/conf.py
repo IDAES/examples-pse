d9c91ad28bc864701c6c4aba3809184d17d239a4
Keith Beattie <KSBeattie@lbl.gov>
2020-08-07 14:39:46 -0700

    examples-dev 1.6_rel branch as of 633d67c in prep for 1.6.1

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = 'DOE Institute for the Design of Advanced Energy Systems (IDAES), 20
 author = 'Dan Gunter'
 
 # The full version, including alpha/beta/rc tags
-release = '1.6.0'
+release = '1.6.1'
 
 
 # For Read the Docs theme
