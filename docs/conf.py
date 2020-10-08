f0b15f1e0228909a71d95266cd5471fdd9e8b5ab
Ludovico Bianchi <lbianchi@lbl.gov>
2020-10-08 16:50:04 -0700

    examples-dev 1.7_rel branch as of af84dd2 in prep for 1.7.0 final

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = 'DOE Institute for the Design of Advanced Energy Systems (IDAES), 20
 author = 'Dan Gunter'
 
 # The full version, including alpha/beta/rc tags
-release = '1.7.0rc1'
+release = '1.7.0'
 
 # For Read the Docs theme
 import sphinx_rtd_theme
