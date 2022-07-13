b24cc3b638ba113790c5b6714caa64cfef0bff9a
Brandon Paul <bpaul@keylogic.com>
2022-07-13 10:18:22 -0700

    Minor text changes

--- src/Examples/UnitModels/Reactors/msr_reaction.py
+++ src/Examples/UnitModels/Reactors/msr_reaction.py
@@ -16,7 +16,7 @@ using Peng-Robinson equation of state.
 
 Author: Brandon Paul
 """
-from pyomo.environ import units as pyunits, exp
+from pyomo.environ import units as pyunits
 
 from idaes.models.properties.modular_properties.base.generic_reaction import \
     ConcentrationForm
@@ -24,17 +24,17 @@ from idaes.models.properties.modular_properties.reactions.dh_rxn import \
     constant_dh_rxn
 from idaes.models.properties.modular_properties.reactions.equilibrium_constant import \
     van_t_hoff
-# empirical expressions for the equilibrium constants as functions of temperature
+from idaes.models.properties.modular_properties.reactions.equilibrium_forms import \
+    power_law_equil
+
+# empirical expressions for equilibrium constants as functions of temperature
 # Source: Int. J. Hydrogen Energy, 42 (2017), pp. 2889-2903
 # to convert to vant hoff form for correlation log(keq) = A/T + B,
 # k_eq_ref = 10^(B + A/T_eq_ref) where T_eq_ref is chosen in K
 # T_eq_ref chosen as 973.15 K (700 C), the case study catalyst temperature
 
-from idaes.models.properties.modular_properties.reactions.equilibrium_forms import \
-    power_law_equil
-
 # For this example, the thermophysical properties are imported from the
-# IDAES natural gas property package (located in the power generation directory)
+# IDAES natural gas property package (located in the power generation folder)
 
 config_dict = {
     "base_units": {"time": pyunits.s,
@@ -53,10 +53,10 @@ config_dict = {
                "equilibrium_constant": van_t_hoff,
                "equilibrium_form": power_law_equil,
                "concentration_form": ConcentrationForm.moleFraction,
-               "parameter_data": {  # calculated from log(keq) = -26830/T + 30.114
-                    "dh_rxn_ref": (206000, pyunits.J/pyunits.mol),  # 223077
-                    "k_eq_ref": (12.727, None),
-                    "T_eq_ref": (973.15, pyunits.K)}},
+               "parameter_data": {  # from log(keq) = -26830/T + 30.114
+                   "dh_rxn_ref": (206000, pyunits.J/pyunits.mol),  # 223077
+                   "k_eq_ref": (12.727, None),
+                   "T_eq_ref": (973.15, pyunits.K)}},
 
         #  Water Gas Shift Reaction
         "E2": {"stoichiometry": {("Vap", "CH4"): 0,
@@ -68,7 +68,7 @@ config_dict = {
                "equilibrium_constant": van_t_hoff,
                "equilibrium_form": power_law_equil,
                "concentration_form": ConcentrationForm.moleFraction,
-               "parameter_data": {# calculated from log(keq) = 4400/T -4.036
-                    "dh_rxn_ref": (-41000, pyunits.J/pyunits.mol),  # -36584
-                    "k_eq_ref": (1.6248, None),
-                    "T_eq_ref": (973.15, pyunits.K)}}}}
+               "parameter_data": {  # from log(keq) = 4400/T -4.036
+                   "dh_rxn_ref": (-41000, pyunits.J/pyunits.mol),  # -36584
+                   "k_eq_ref": (1.6248, None),
+                   "T_eq_ref": (973.15, pyunits.K)}}}}
