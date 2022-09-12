d0d956a7ab5445a0891a80bea412840f8f65fd5c
Brandon Paul <bpaul@keylogic.com>
2022-09-12 09:05:13 -0700

    Add setup.py to enable installation as idaes_examples namespace package

--- pkg/idaes_examples/common/methanol/flowsheet_w_recycle.py
+++ pkg/idaes_examples/common/methanol/flowsheet_w_recycle.py
@@ -44,9 +44,9 @@ from idaes.models.properties.modular_properties.base.generic_property import \
 from idaes.models.properties.modular_properties.base.generic_reaction import \
     GenericReactionParameterBlock
 
-import methanol_ideal_VLE as thermo_props_VLE
-import methanol_ideal_vapor as thermo_props_vapor
-import methanol_reactions as reaction_props
+from idaes_examples.common.methanol import ideal_VLE as thermo_props_VLE
+from idaes_examples.common.methanol import ideal_vapor as thermo_props_vapor
+from idaes_examples.common.methanol import reactions as reaction_props
 
 from idaes.models.unit_models import (
     Feed,
