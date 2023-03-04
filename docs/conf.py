644e4f997dc510e2a374cb80eb59f9f646f259d6
Ludovico Bianchi <lbianchi@lbl.gov>
2023-03-03 18:24:25 -0600

    2.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = 'DOE Institute for the Design of Advanced Energy Systems (IDAES), 20
 author = 'Dan Gunter'
 
 # The full version, including alpha/beta/rc tags
-release = '2.0.0dev0'
+release = '2.0.0'
 
 
 # For Read the Docs theme
